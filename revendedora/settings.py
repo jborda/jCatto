8d0ae31909748c919e0dde6106a8ef73abd97275
jborda <emaildotaborda@gmail.com>
2013-03-19 23:51:34 -0300

    [T-13] Estrutura inicial do projeto  - Organizando pacotes  - Projeto Eclipse
    [T-19] Criar estrutura de template
     - Criando estrutura inicial das páginas

--- revendedora/settings.py
+++ revendedora/settings.py
@@ -102,12 +102,13 @@ MIDDLEWARE_CLASSES = (
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
-ROOT_URLCONF = 'revendedora.urls'
+ROOT_URLCONF = 'urls'
 
 # Python dotted path to the WSGI application used by Django's runserver.
-WSGI_APPLICATION = 'revendedora.wsgi.application'
+WSGI_APPLICATION = 'wsgi.application'
 
 TEMPLATE_DIRS = (
+    "pages"
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
--- revendedora/urls.py
+++ revendedora/urls.py
@@ -5,6 +5,7 @@ from django.conf.urls import patterns, include, url
 # admin.autodiscover()
 
 urlpatterns = patterns('',
+    url(r'^$', 'views.index'),
     # Examples:
     # url(r'^$', 'revendedora.views.home', name='home'),
     # url(r'^revendedora/', include('revendedora.foo.urls')),
